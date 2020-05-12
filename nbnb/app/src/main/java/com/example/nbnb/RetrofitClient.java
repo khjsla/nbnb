fdcd0978957e2048f90597d61d8d43c0af5f0765
khj <rkdguswlsla@gmail.com>
2020-05-12 20:38:36 +0900

    Hello Github

---
+++ nbnb/app/src/main/java/com/example/nbnb/RetrofitClient.java
@@ -0,0 +1,22 @@
+package com.example.nbnb;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private final static String BASE_URL = "http://ec2-54-180-116-73.ap-northeast-2.compute.amazonaws.com:3000"; //:3306 빼봤음
+    private static Retrofit retrofit = null;
+
+    private RetrofitClient() {
+    }
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
